a6aa5639e2f2a6deb066c92cb0dbf1f71762ca58
tahakh03 <khuwaja.taha@mail.utoronto.ca>
2025-09-06 15:39:10 -0400

    set base option to repo name

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/Personal-Website/', // Replace 'repository-name' with your GitHub repository name
   plugins: [react()],
 })
